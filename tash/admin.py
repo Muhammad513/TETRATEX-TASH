11d8a997b60055641978592cc1dec11048d018ab
MuhammadBobur <121751236+MuhammadDevAgro@users.noreply.github.com>
2023-02-22 22:59:11 +0300

    Dashboard qilindi

--- tash/admin.py
+++ tash/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
-
+from .models import Punkt,Partiya,Tashish,Firma,Transport
 # Register your models here.
+class PunktAdmin(admin.ModelAdmin):
+    list_display = ('id','name', 'bux')
+
+class PartiyaAdmin(admin.ModelAdmin):
+    list_display = ('id','partiya', 'bunt',"nav","sort",'snif',"sofVazn",'ifloslik',"xisobiy",'namlik','kond')
+
+
+class TashishAdmin(admin.ModelAdmin):
+    
+    @admin.display(description="Tashish")
+    def admin_date(self,obj):
+        return obj.date.strftime('%d-%m-%Y')
+    
+    
+    list_display = ('id','admin_date', 'nak_num',"transport","sofVazn",'partiya',"ifloslik",'namlik',"xisobiy",'kond')
+
+
+class FirmaAdmin(admin.ModelAdmin):
+    list_display = ('id','name')
+
+class TransportAdmin(admin.ModelAdmin):
+    list_display = ('id','firma', 'rusum',"tr_num",)
+
+
+
+
+
+
+
+
+admin.site.register(Punkt,PunktAdmin) 
+admin.site.register(Partiya,PartiyaAdmin)    
+admin.site.register(Tashish,TashishAdmin)
+admin.site.register(Transport,TransportAdmin)
+admin.site.register(Firma,FirmaAdmin)    
+
+
+
+
+
+
+ 
--- tash/models.py
+++ tash/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Punkt(models.Model):
+    name=models.CharField(max_length=20)
+    bux=models.OneToOneField(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Partiya(models.Model):
+    xosil=models.CharField(max_length=4)
+    partiya=models.CharField(max_length=7)
+    bunt=models.CharField(max_length=20)
+    nav=models.CharField(max_length=30)
+    sort=models.CharField(max_length=1)
+    snif=models.CharField(max_length=1)
+    sofVazn=models.FloatField()
+    xisobiy=models.FloatField()
+    kond=models.FloatField()
+    ifloslik=models.FloatField(blank=True)
+    namlik=models.FloatField(blank=True)
+
+
+    def save(self,*args,**kwargs):
+        self.ifloslik=round(100-((self.xisobiy*98)/self.sofVazn),1)
+        self.namlik=round(((self.xisobiy*109)/self.kond)-100,1)
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return str(self.partiya)    
+
+class Firma(models.Model):
+    name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.name)
+
+class Transport(models.Model):
+    firma=models.ForeignKey('Firma',on_delete=models.CASCADE)
+    rusum=models.CharField(max_length=20)
+    tr_num=models.CharField(max_length=20)
+    
+    def __str__(self):
+        return str(self.tr_num)
+
+
+
+class Tashish(models.Model):
+    date=models.DateField(auto_now=True)
+    nak_num=models.CharField(max_length=20)
+    transport=models.ForeignKey('Transport',on_delete=models.CASCADE)
+    sofVazn=models.FloatField()
+    partiya=models.ForeignKey("Partiya",on_delete=models.CASCADE)
+    ifloslik=models.FloatField()
+    namlik=models.FloatField()
+    xisobiy=models.FloatField(blank=True)
+    kond=models.FloatField(blank=True)
+    
+
+    
+    def save(self,*args,**kwargs):
+        self.xisobiy=round((self.sofVazn*((100-self.ifloslik)/98)),1)
+        self.kond=round((self.sofVazn*((100-self.ifloslik)/98))*(109/(100+self.namlik)),1)
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return str(self.date)
